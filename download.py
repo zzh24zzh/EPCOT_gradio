45569f063a3af265f418cdb79d7a9171bc1ed814
张振豪 <zhangzhenhao@gous-MacBook-Pro-2.local>
2022-11-16 17:16:43 -0500

    download

--- download.py
+++ download.py
@@ -5,14 +5,14 @@ def download_models():
     gdown.download_folder('https://drive.google.com/drive/u/1/folders/1oqrRToBfzjfkDoQzy8SGRY1x2qGhAwj1')
 
 def download_examples():
-    gdown.download('https://drive.google.com/file/d/1QW4hVjgxZZeQYXO0XyWhALhLyy-fEpPP/view?usp=share_link',output='examples/chr11.npz')
-    gdown.download('https://drive.google.com/file/d/1EZ-gE2Gs2hRpNIali1dSOijOkXYqx4Sv/view?usp=share_link',
+    gdown.download('https://drive.google.com/uc?id=1QW4hVjgxZZeQYXO0XyWhALhLyy-fEpPP',output='examples/chr11.npz')
+    gdown.download('https://drive.google.com/uc?id=1EZ-gE2Gs2hRpNIali1dSOijOkXYqx4Sv',
                    output='examples/GM12878_atac_chr11.npz')
 
 def download_refseq_hg38():
     if not os.path.exists('refSeq'):
         os.mkdir('refSeq')
-    gdown.download('https://drive.google.com/file/d/1iqOKKNFwjl9hMZovxxhG-t_Y1MZw45R0/view?usp=share_link',
+    gdown.download('https://drive.google.com/uc?id=1iqOKKNFwjl9hMZovxxhG-t_Y1MZw45R0',
                    output='refSeq/hg38.tar.gz')
     os.system('tar -xvf refSeq/hg38.tar.gz -C refSeq/ hg38/chr11.npz')
 
