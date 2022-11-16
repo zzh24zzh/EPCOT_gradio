e9e115a178811a12865f0c045fe9fd903640e8fd
张振豪 <zhangzhenhao@gous-MacBook-Pro-2.local>
2022-11-15 23:10:00 -0500

    change

--- process_atac.py
+++ process_atac.py
@@ -1,23 +1,28 @@
 import pyBigWig
 import numpy as np
-import pickle,os
+import pickle,os,argparse
 from scipy.sparse import csr_matrix,save_npz
 from optparse import OptionParser
 
+def parser_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--inputFileName', '-i',
+                          help='Input prediction file name',
+                          required=True)
+    parser.add_argument('--prefix', '-p',default='',help='prefix')
+    parser.add_argument('--out_dir', '-o',
+                       help='Output directory',
+                       required=True)
+    args = parser.parse_args()
+    return args
+
+
 def atac_processing():
-    usage = 'usage: %prog [options] <bigWig_file> <prefix>'
-    parser = OptionParser(usage)
-    (options, args) = parser.parse_args()
-    if len(args) < 1:
-        parser.error('Please justify the DNase-seq bigwig file path')
-    else:
-        dnase_file = args[0]
-    if len(args)==2:
-        prefix=args[1]
-    else:
-        prefix=''
-    os.mkdir('atacseq')
-    bw = pyBigWig.open(dnase_file)
+    args=parser_args()
+    if not os.path.exists(args.out_dir):
+        os.mkdir(args.out_dir)
+
+    bw = pyBigWig.open(args.inputFileName)
     for chrom, length in bw.chroms().items():
         try:
             if chrom == 'chrX':
@@ -31,6 +36,6 @@ def atac_processing():
         for interval in intervals:
             temp[interval[0]:interval[1]] = interval[2]
         seq_length = length // 1000 * 1000
-        save_npz('atacseq/%s_atac_chr%s.npz'%(prefix,chr),csr_matrix(temp.astype('float32')[:seq_length]))
+        save_npz(os.path.join(args.out_dir,'%s_atac_chr%s.npz'%(args.prefix,chr)),csr_matrix(temp.astype('float32')[:seq_length]))
 if __name__ == '__main__':
     atac_processing()
